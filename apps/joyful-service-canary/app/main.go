dac696c9a90b46fb71a0dc6df98e17a2ac2977bb
Victor Efedi <169038469+victor-efedi-sympower@users.noreply.github.com>
2025-05-12 17:07:45 +0200

    Added health checks

--- apps/joyful-service-canary/app/main.go
+++ apps/joyful-service-canary/app/main.go
@@ -11,15 +11,23 @@ func main() {
 
 	// Serve static files
 	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.Handle("/static/", logMiddleware(http.StripPrefix("/static/", fs)))
 
-	http.HandleFunc("/", home)
-	http.HandleFunc("/ping", pingHandler) // Add the ping handler
+	http.Handle("/", logMiddleware(http.HandlerFunc(home)))
+	http.Handle("/ping", logMiddleware(http.HandlerFunc(pingHandler))) // Add the ping handler
 
 	log.Printf("Starting server on %s...\n", port)
 	if err := http.ListenAndServe(port, nil); err != nil {
 		log.Fatalf("Error: %v\n", err)
 	}
+}
+
+// Middleware to log requests
+func logMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Request: %s %s from %s", r.Method, r.URL.Path, r.RemoteAddr)
+		next.ServeHTTP(w, r)
+	})
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
