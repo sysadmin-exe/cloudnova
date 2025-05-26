ff383f251184cf1394d482821976daba6d47baf8
Victor Efedi <169038469+victor-efedi-sympower@users.noreply.github.com>
2025-05-26 10:21:54 +0200

    Fixed Happy v2

--- apps/happy-service-bg/app/main.go
+++ apps/happy-service-bg/app/main.go
@@ -23,7 +23,7 @@ func main() {
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-	file := "static/index_v2.html"
+	file := "static/index_v1.html"
 
 	if _, err := os.Stat(file); os.IsNotExist(err) {
 		http.Error(w, "404 - Page Not Found", http.StatusNotFound)
